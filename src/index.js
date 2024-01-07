30305db351c65ae9b04e4d2bcc4b339f4a95ec55
alexandraverasalinas <alexandraverasalinas@gmail.com>
2024-01-07 13:45:40 -0500

    default

--- src/index.js
+++ src/index.js
@@ -54,4 +54,6 @@ function refreshWeather(response) {
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  searchCity("Paris");
   
